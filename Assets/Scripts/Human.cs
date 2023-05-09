1e6b0a9e17931abc65dce9bdea7e681744b443e7
hashbert <obert.chen@gmail.com>
2023-05-09 17:53:53 -0400

    animation on human fixed, warning sign added

--- Assets/Scripts/Human.cs
+++ Assets/Scripts/Human.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 
 public class Human : MonoBehaviour
 {
+    private bool _needsFood;
     [SerializeField] private InputActionReference _select;
     // Start is called before the first frame update
 
